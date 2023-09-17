2a75a0cf2665247f60ec0f95f4d291774f4e0029
Akmal <akmalagzamov95@gmail.com>
2023-09-17 17:36:30 +0500

    column and row

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,98 +1,38 @@
 import 'package:flutter/material.dart';
 
-class AccaountPage extends StatelessWidget {
-  const AccaountPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 30,
-                backgroundImage:
-                    NetworkImage('https://source.unsplash.com/random'),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                children: [Text('10'), Text('Preferred list')],
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                children: [Text('10'), Text('Followed stores')],
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Column(
-                children: [Text('10'), Text('Vaucher List')],
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 40,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      appBar: AppBar(title: Text('App')),
+      body: Container(
+        child: Row(
           children: [
-            Column(
-              children: [Icon(Icons.receipt), Text('To pay')],
-            ),
-            Column(
-              children: [Icon(Icons.send), Text('To send')],
-            ),
-            Column(
-              children: [Icon(Icons.delivery_dining), Text('To recive')],
-            ),
-            Column(
-              children: [Icon(Icons.reviews_outlined), Text('Review')],
-            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.red,
+                )
+              ),
+            ),Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: 90,
+                    height: 160,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            )
           ],
         ),
-        SizedBox(
-          height: 400.0,
-          width: 390.0,
-          child: GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 338.0,
-                          width: 338.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      'https://previews.123rf.com/images/artemstepanov/artemstepanov1611/artemstepanov161100172/66430429-big-sale-poster-with-big-sale-text-advertising-boom-and-red-banner-template.jpg'))),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        )
-      ],
+      ),
     );
   }
 }
