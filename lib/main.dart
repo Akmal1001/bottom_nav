4ac38c640ce513882d87083655d02ab569e9a46f
Akmal <akmalagzamov95@gmail.com>
2023-09-17 18:41:40 +0500

    statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:palach/view/home_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-
-  runApp(
-    EasyLocalization(
-        supportedLocales: const [Locale('eng'), Locale('uzb'), Locale('rus')],
-        path: 'lib/core/lang',
-        child: const MyApp()),
-  );
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,34 +1,35 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int son = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('App')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text("hello".tr(),style: const TextStyle(fontSize: 50),),
-            
-          ),
-          FloatingActionButton(onPressed: (){
-            context.setLocale(Locale('eng'));
-          },child: Text('Eng')),
-          FloatingActionButton(onPressed: (){
-            context.setLocale(Locale('rus'));
-          },child: Text('Uzb')),
-          FloatingActionButton(onPressed: (){
-            context.setLocale(Locale('uzb'));
-          },child: Text('Rus'))
-        ],
+      appBar: AppBar(title: Text('APP')),
+      body: Center(
+        child: Text(son.toString(),
+            style: TextStyle(fontSize: 50, color: Colors.deepPurple)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          orttit();
+          setState(() {});
+        },
+        child: Text('PRESS'),
       ),
     );
+  }
+
+  orttit() {
+    son++;
+    print(son);
   }
 }
